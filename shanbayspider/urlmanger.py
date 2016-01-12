f8b1810a4df93e660b61bc4bbf65a690afeda877
WillleePang <pangwilllee@hotmail.com>
2016-01-12 14:44:30 +0800

    Update urlmanger.py

--- shanbayspider/urlmanger.py
+++ shanbayspider/urlmanger.py
@@ -3,27 +3,5 @@
 
 
 class UrlManager(object):
-    def __init__(self):
-        self.new_urls = set()
-        self.old_urls = set()
-
-    def add_new_url(self, new_url):
-        if new_url is None:
-            return
-        if new_url not in self.new_urls or new_url not in self.old_urls:
-            self.new_urls.add(new_url)
-
-    def add_new_urls(self, urls):
-        if urls is None or len(urls) == 0:
-            return
-        for url in urls:
-            self.addNewUrl(url)
-        pass
-
-    def has_new_url(self):
-        return len(self.new_urls) != 0
-
-    def get_new_url(self):
-        new_url = self.new_urls.pop()
-        self.old_urls.add(new_url)
-        return new_url
+    def get_shanbay_url(self, shanbay_url, i):
+        return shanbay_url + '?page=' + str(i)
