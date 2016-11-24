1897924a4fe125473308fa0a3088e931ee0b6e7a
willleepang <pangwilllee@hotmail.com>
2016-11-24 16:00:44 +0800

    copy template-engine complete!

--- template-engine/templite.py
+++ template-engine/templite.py
@@ -4,6 +4,11 @@
 import re
 
 
+class TempliteSyntaxError(ValueError):
+    """Raised when a template has a syntax error."""
+    pass
+
+
 class CodeBuilder(object):
     """Build source code conveniently"""
 
@@ -140,10 +145,105 @@ class Templite(object):
                             self._expr_code(words[3])
                         )
                     )
+                    code.indent()
+                elif words[0].startswith('end'):
+                    # Endsomething. Pop the ops stack.
+                    if len(words) != 1:
+                        self._syntax_error("Don't understand end", token)
+                    end_what = words[0][3:]
+                    if not ops_stack:
+                        self._syntax_error("Too many ends", token)
+                    start_what = ops_stack.pop()
+                    if start_what != end_what:
+                        self._syntax_error("Mismatched end tag", end_what)
+                    code.dedent()
+                else:
+                    self._syntax_error("Don't understand tag", words[0])
+            else:
+                if token:
+                    buffered.append(repr(token))
+        else:
+            # Literal content. If it isn't empty, output it.
+            if token:
+                buffered.append(repr(token))
+
+        if ops_stack:
+            self._syntax_error("Unmatched action tag", ops_stack[-1])
+
+        flush_output()
+
+        for var_name in self.all_vars - self.loop_vars:
+            vars_code.add_line("c_%s = context[%r]" % (var_name, var_name))
+
+        code.add_line("return ''.join(result)")
+        code.dedent()
+        self._render_function = code.get_globals()['render_function']
+
+    def _expr_code(self, expr):
+        """Generate a Python expression for `expr`."""
+        if "|" in expr:
+            pipes = expr.split("|")
+            code = self._expr_code(pipes[0])
+            for func in pipes[1:]:
+                self.variable(func, self.all_vars)
+                code = "c_%s(%s)" % (func, code)
+        elif "." in expr:
+            dots = expr.split(".")
+            code = self._expr_code(dots[0])
+            args = ", ".join(repr(d) for d in dots[1:])
+            code = "do_dots(%s, %s)" % (code, args)
+        else:
+            self._variable(expr, self.all_vars)
+            code = "c_%s" % expr
+        return code
+
+    def _syntax_error(self, msg, thing):
+        """Raise a syntax error using `msg`, and showing `thing`."""
+        raise TempliteSyntaxError("%s: %r" % (msg, thing))
+
+    def _variable(self, name, vars_set):
+        """Track that `name` is used as a variable.
+        Adds the name to `vars_set`, a set of variable names.
+        Raises an syntax error if `name` is not a valid name.
+        """
+        if not re.match(r"[_a-zA-Z][_a-zA-Z0-9]*$", name):
+            self._syntax_error("Not a valid name", name)
+        vars_set.add(name)
+
+    def render(self, context=None):
+        """Render this template by applying it to `context`.
+        `context` is a dictionary of values to use in this rendering.
+        """
+        # Make the complete context we'll use.
+        render_context = dict(self.context)
+        if context:
+            render_context.update(context)
+        return self._render_function(render_context, self._do_dots)
+
+    def _do_dots(self, value, *dots):
+        """Evaluate dotted expressions at runtimes."""
+        for dot in dots:
+            try:
+                value = getattr(value, dot)
+            except AttributeError:
+                value = value[dot]
+            if callable(value):
+                value = value()
+        return value
+
+
+def _do_dots(value, *dots):
+    """Evaluate dotted expressions at runtimes."""
+    for dot in dots:
+        try:
+            value = getattr(value, dot)
+        except AttributeError:
+            value = value[dot]
+        if callable(value):
+            value = value()
+    return value
 
 
 if __name__ == '__main__':
-    text = "<p>Topics for {{name}}: {% for t in topics %}{#worinimei#}{{t}}, {% endfor %}</p>"
-    tokens = re.split(r"(?s)({{.*?}}|{%.*?%}|{#.*?#})", text)
-    for token in tokens:
-        print token
+    x = {"y": {"z": 11}}
+    print _do_dots(x, "y", "z")
